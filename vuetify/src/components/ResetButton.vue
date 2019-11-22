477507b7398039529b312067aad44420d3fd217e
smicle <smicle@pepabo.com>
2019-11-22 18:28:12 +0900

    Add reset

---
+++ vuetify/src/components/ResetButton.vue
@@ -0,0 +1,25 @@
+<template>
+  <button @click="onClick">Reset</button>
+</template>
+
+<script lang="ts">
+import {Component, Emit, Prop, Vue} from 'vue-property-decorator'
+
+@Component
+export default class ResetButton extends Vue {
+  @Prop({required: true})
+  public initialValue!: string
+
+  /** モデルバインドのために記述必須 */
+  @Prop()
+  public value!: string
+
+  /** モデルバインドのために記述必須 */
+  @Emit()
+  public input(value: string) {}
+
+  public onClick() {
+    this.input(this.initialValue)
+  }
+}
+</script>
--- vuetify/src/views/About.vue
+++ vuetify/src/views/About.vue
@@ -1,23 +1,31 @@
 <template>
-  <div class="about">
-    <MyButton :greet="greetText" @click="onMyButtonClicked"></MyButton>
+  <div class="home">
+    <p>{{ greetText }}</p>
+    <p>
+      <MyButton :greet="greetText" @click="onMyButtonClicked"></MyButton>
+    </p>
+    <p>
+      <ResetButton initialValue="Hello" v-model="greetText"></ResetButton>
+    </p>
   </div>
 </template>
 
 <script lang="ts">
 import {Component, Vue} from 'vue-property-decorator'
 import MyButton from '@/components/MyButton.vue'
+import ResetButton from '@/components/ResetButton.vue'
 
 @Component({
   components: {
     MyButton,
+    ResetButton,
   },
 })
-export default class About extends Vue {
+export default class Home extends Vue {
   public greetText: string = 'Hello'
 
   public onMyButtonClicked() {
-    this.greetText = this.greetText === 'こんにちは' ? 'Hello' : 'こんにちは'
+    this.greetText = 'こんにちは'
   }
 }
 </script>
